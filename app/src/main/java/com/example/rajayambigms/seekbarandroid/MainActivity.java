25ed6ec97a806a4f35e67fa05f47b21ce43f273b
Rajendan <rajendranmca2010@gmail.com>
2019-02-19 19:21:27 -0500

    Initial commit

---
+++ app/src/main/java/com/example/rajayambigms/seekbarandroid/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.rajayambigms.seekbarandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private TextView textView;
+    private SeekBar seekBar;
+    private String progressCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView)findViewById(R.id.textView);
+        seekBar =(SeekBar)findViewById(R.id.seekBar);
+
+        textView.setText(String.valueOf(seekBar.getProgress()));
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                progressCount = Integer.toString(progress);
+
+                textView.setText(progressCount);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
